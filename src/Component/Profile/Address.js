3ae6eca5807f11aa066d8b0ddd2f346ab33cdc19
bbelgace <rina.sab.ben@gmail.com>
2021-10-24 15:08:52 +0200

    component

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Address = () => {
+  return (
+    <div>
+      <h3>sité tahrir redayef gafsa </h3>
+    </div>
+  );
+};
+
+export default Address;
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfilePhoto = () => {
+  return (
+    <div>
+      <img src="./public/imge.jpg" alt="img" />
+    </div>
+  );
+};
+
+export default ProfilePhoto;
